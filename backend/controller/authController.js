eb3f7d3bee0f325a10566f77142fe446ca3125a2
hempalariya <112241094+hempalariya@users.noreply.github.com>
2025-10-02 13:43:36 +0530

    backend login logic is done

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -15,5 +15,12 @@ export async function registerUser (req, res){
 }
 
 export async function loginUser (req,res){
-    
-}
+    try {
+        const {email, password} = req.body
+        const user = await User.find({email, password})
+        res.status(200).json(user)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: error.message})
+    }
+}     
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -4,6 +4,6 @@ const authRouter = express.Router()
 
 authRouter.post('/register', registerUser)
 
-
+authRouter.post('/login', loginUser)
 export default authRouter
 
