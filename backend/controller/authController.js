78866e576b7d5f1b1004f0189922aa0095cb7eac
hempalariya <112241094+hempalariya@users.noreply.github.com>
2025-10-03 08:59:29 +0530

    bcrypt logic

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,26 +1,33 @@
-import User from '../models/User.js'
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
 
-export async function registerUser (req, res){
-    try {
-        const {username, email, password} = req.body
-        const newUser = await User.create({
-            username,
-            email,
-            password
-        })
-        res.status(201).json(newUser)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
+export async function registerUser(req, res) {
+  try {
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
-export async function loginUser (req,res){
-    try {
-        const {email, password} = req.body
-        const user = await User.find({email, password})
-        res.status(200).json(user)
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({message: error.message})
-    }
-}     
+export async function loginUser(req, res) {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "Invalid credentials" });
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+}
